0e71e3cbef469ebd3c18adcacc70db398a8a723e
André Miranda <andrelmmiranda@gmail.com>
2022-05-11 15:24:23 -0300

    adicionando método construtor

---
+++ index.js
@@ -0,0 +1,61 @@
+
+// Atividade Prática Básico
+// Título da Prática: Condições Meteorológicas
+// Objetivos: Utilizando os conceitos de programação com JavaScript e Node.js,
+// verificar as temperaturas durante um dia de medição.
+// Materiais, Métodos e Ferramentas: JavaScript e Node.js
+// ________________________________________________________________________
+// Atividade prática do projeto:
+// Escreva um programa em JavaScript no Node.js que ajuda o INPE, que possui um
+// controle histórico das condições meteorológicas de um ano, a organizar
+// informações de uma estação meteorológica. Considere um vetor com as seguintes
+// informações da estação meteorológica:
+// dia e mês do registro
+// temperatura máxima
+// temperatura mínima
+// indicador se choveu no dia
+// O programa deverá determinar quantos dias choveu nos 365 dias do ano e a média
+// da temperatura máxima e mínima de um determinado mês indicados pelo usuário.
+// Estruture a solução conforme sugerido a seguir.
+// Passo 1. Crie um registro para conter as informações da estação
+// meteorológica.
+// Passo 2. Um módulo função que recebe um vetor de registros. Este módulo
+// deverá ser armazenado com as informações necessárias e preenchidas com as
+// informações da estação meteorológica e retornar esse vetor.
+
+// Passo 3. Uma função que receba como parâmetro um vetor com as
+// informações da estação meteorológica, determine e retorne quantos dias choveu
+// nos últimos 365 dias.
+// Passo 4. Um procedimento que receba como parâmetro um vetor com as
+// informações da estação meteorológica e determine e apresente a média da
+// temperatura máxima e a média da temperatura mínima no período de 365 dias do
+// ano.
+// Passo 5. Uma função que receba como parâmetro um vetor com informações
+// da estação meteorológica e ordena as informações da menor para a maior
+// temperatura, retornando este vetor.
+// Passo 6. Um procedimento que receba como parâmetro um vetor com
+// informações da estação meteorológica e mostre todas as informações ao usuário
+// final.
+// Passo 7. O método principal que:
+// • Cria um vetor com capacidade para armazenar as informações de 365 dias da
+// estação meteorológica.
+// • Com o auxílio da questão 2, preencher o vetor com as informações das
+// temperaturas fornecidas pelo usuário.
+// • Com o auxílio da questão 3, determinar, retornar e apresentar quantos dias
+// choveu nos últimos 365 dias.
+// • Com o auxílio da questão 4, determinar e apresentar a média da temperatura
+// máxima e a média da temperatura mínima dos últimos 365 dias.
+
+// • Com o auxílio da questão 5, ordenar o vetor da temperatura mínima para a
+// temperatura máxima.
+// • Com o auxílio da questão 6, acionar o módulo para mostrar as informações
+// ordenadas para o usuário.
+
+class metInfo{
+    constructor(){
+        dia e mês do registro
+        temperatura máxima
+        temperatura mínima
+        indicador se choveu no dia
+    }
+}
