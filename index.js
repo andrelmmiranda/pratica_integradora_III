08cf2bb6aa7a64b2f1e4f93182ba2169cbbff0d7
André Miranda <andrelmmiranda@gmail.com>
2022-05-11 21:41:59 -0300

    criação de funções

--- index.js
+++ index.js
@@ -1,61 +1,79 @@
-
-// Atividade Prática Básico
-// Título da Prática: Condições Meteorológicas
-// Objetivos: Utilizando os conceitos de programação com JavaScript e Node.js,
-// verificar as temperaturas durante um dia de medição.
-// Materiais, Métodos e Ferramentas: JavaScript e Node.js
-// ________________________________________________________________________
-// Atividade prática do projeto:
-// Escreva um programa em JavaScript no Node.js que ajuda o INPE, que possui um
-// controle histórico das condições meteorológicas de um ano, a organizar
-// informações de uma estação meteorológica. Considere um vetor com as seguintes
-// informações da estação meteorológica:
-// dia e mês do registro
-// temperatura máxima
-// temperatura mínima
-// indicador se choveu no dia
-// O programa deverá determinar quantos dias choveu nos 365 dias do ano e a média
-// da temperatura máxima e mínima de um determinado mês indicados pelo usuário.
-// Estruture a solução conforme sugerido a seguir.
-// Passo 1. Crie um registro para conter as informações da estação
-// meteorológica.
-// Passo 2. Um módulo função que recebe um vetor de registros. Este módulo
-// deverá ser armazenado com as informações necessárias e preenchidas com as
-// informações da estação meteorológica e retornar esse vetor.
-
-// Passo 3. Uma função que receba como parâmetro um vetor com as
-// informações da estação meteorológica, determine e retorne quantos dias choveu
-// nos últimos 365 dias.
-// Passo 4. Um procedimento que receba como parâmetro um vetor com as
-// informações da estação meteorológica e determine e apresente a média da
-// temperatura máxima e a média da temperatura mínima no período de 365 dias do
-// ano.
-// Passo 5. Uma função que receba como parâmetro um vetor com informações
-// da estação meteorológica e ordena as informações da menor para a maior
-// temperatura, retornando este vetor.
-// Passo 6. Um procedimento que receba como parâmetro um vetor com
-// informações da estação meteorológica e mostre todas as informações ao usuário
-// final.
-// Passo 7. O método principal que:
-// • Cria um vetor com capacidade para armazenar as informações de 365 dias da
-// estação meteorológica.
-// • Com o auxílio da questão 2, preencher o vetor com as informações das
-// temperaturas fornecidas pelo usuário.
-// • Com o auxílio da questão 3, determinar, retornar e apresentar quantos dias
-// choveu nos últimos 365 dias.
-// • Com o auxílio da questão 4, determinar e apresentar a média da temperatura
-// máxima e a média da temperatura mínima dos últimos 365 dias.
-
-// • Com o auxílio da questão 5, ordenar o vetor da temperatura mínima para a
-// temperatura máxima.
-// • Com o auxílio da questão 6, acionar o módulo para mostrar as informações
-// ordenadas para o usuário.
+// Fiz com apenas 20 campos preenchidos, pois preencher 365 campo seria demasiadamente cansativo e a solução produzida atende aos propósitos solicitados, funcionando com mais ou menos objetos inseridos em 'arrayDados'
 
 class metInfo{
-    constructor(){
-        dia e mês do registro
-        temperatura máxima
-        temperatura mínima
-        indicador se choveu no dia
+    constructor(diaMes, tempMax, tempMin, choveu){
+        this.diaMes = diaMes;
+        this.tempMax = tempMax;
+        this.tempMin = tempMin;
+        this.choveu = choveu;
     }
 }
+
+const metInfoBuilder = (diaMes, tempMax, tempMin, choveu)=> new metInfo(diaMes, tempMax, tempMin, choveu);
+
+const arrayDados = [
+    metInfoBuilder('01/01', 32, 27, false),
+    metInfoBuilder('02/01', 32, 24, true),
+    metInfoBuilder('03/01', 30, 23, true),
+    metInfoBuilder('04/01', 28, 22, true),
+    metInfoBuilder('05/01', 27, 24, true),
+    metInfoBuilder('06/01', 27, 24, false),
+    metInfoBuilder('07/01', 28, 27, false),
+    metInfoBuilder('08/01', 28, 24, false),
+    metInfoBuilder('09/01', 31, 25, false),
+    metInfoBuilder('10/01', 32, 27, false),
+    metInfoBuilder('11/01', 34, 26, false),
+    metInfoBuilder('12/01', 32, 27, false),
+    metInfoBuilder('13/01', 31, 25, false),
+    metInfoBuilder('14/01', 32, 24, false),
+    metInfoBuilder('15/01', 32, 27, false),
+    metInfoBuilder('16/01', 30, 27, false),
+    metInfoBuilder('17/01', 27, 21, true),
+    metInfoBuilder('18/01', 27, 19, true),
+    metInfoBuilder('19/01', 28, 21, true),
+    metInfoBuilder('20/01', 26, 22, true)
+];
+
+
+const arrayTempMax = (arrayDados)=> arrayDados.map(temp => temp.tempMax);
+
+const arrayTempMin = (arrayDados)=> arrayDados.map(temp => temp.tempMin);
+
+const sortTemp = (arrayDados)=> arrayDados.sort((a, b)=> a - b);
+
+const tempMax = (arrayDados)=> arrayTempMax(arrayDados).reduce((acc, el)=> el + acc);
+
+const tempMin = (arrayDados)=> arrayTempMin(arrayDados).reduce((acc, el)=> el + acc);
+
+const mediaTemp = (totalTemp)=> totalTemp / arrayDados.length;
+
+const choveu = (arrayDados)=> arrayDados.filter(choveu => choveu.choveu === true).length;
+
+const montaArray = (arrayDados)=>{
+    const array = new Array(arrayDados.length);
+
+    for(let data in arrayDados){
+        array[data] = arrayDados[data];
+    }
+    return array;
+}
+
+const output = (sortTempMax, SortTempMin, mediaTempMax, mediaTempMin, diasDeChuva)=>{
+    console.log(
+`
+Temp Máxima ordenadas: ${sortTempMax}
+Temp Mínima ordenadas: ${SortTempMin}
+Média Temp Máxima: ${mediaTempMax}°
+Média Temp Mínima: ${mediaTempMin}°
+Qtde dias com chuva: ${diasDeChuva} ${diasDeChuva === 0 ? '': diasDeChuva === 1 ? 'dia':'dias'}
+`
+)
+}
+
+const main = (arrayDados)=>{
+    const array = montaArray(arrayDados);
+
+    output(sortTemp(arrayTempMax(array)), sortTemp(arrayTempMin(array)), mediaTemp(tempMax(array)), mediaTemp(tempMin(array)), choveu(array));
+}
+
+main(arrayDados);
